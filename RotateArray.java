588cafea17b9a1b191075ff3fd5bf9184bfe5957
Dharani M <dharanimoorthy51@gmail.com>
2026-01-08 21:26:29 +0530

    Rotate Array

---
+++ RotateArray.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotate(nums, k);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        swap(nums, 0, nums.length - 1);
+        swap(nums, 0, k - 1);
+        swap(nums, k, nums.length - 1);
+    }
+    public static void swap(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
